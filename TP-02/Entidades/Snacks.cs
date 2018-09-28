9e68c538d162526f7b035c5e28c714d1f553384f
juaninarvaja <juaninarvaja@gmail.com>
2018-09-28 01:46:51 -0300

    Add files via upload

---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades_2018;
+
+namespace Entidades_2018
+{
+    public class Snacks : Producto
+    {
+        public Snacks(EMarca marca, string codigoDeBarras, ConsoleColor color)
+            : base(codigoDeBarras, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
